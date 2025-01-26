aaf3454aafb48abad50ce189f4df085e3128c17d
Austinll0 <Austinllafever@ymail.com>
2025-01-26 17:35:35 -0500

    debugged hyprland conf

--- flake.nix
+++ flake.nix
@@ -1,42 +1,43 @@
 {
-	description = "A simple NixOS flake";
+    description = "A simple NixOS flake";
 
-	inputs = {
-		# NixOS official package source, using the nixos-24.11 branch here
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-		
-		#hyprland setup
-		hyprland.url = "github:hyprwm/Hyprland";
-		hyprland-plugins = {
-			url = "github:hyprwm/hyprland-plugins";
-			inputs.hyprland.follows = "hyprland";
-		};
-		# home manager setup 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-24.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		
-	};
+    inputs = {
+        # NixOS official package source, using the nixos-24.11 branch here
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        
+        #hyprland setup
+        hyprland.url = "github:hyprwm/Hyprland";
+        hyprland-plugins = {
+                url = "github:hyprwm/hyprland-plugins";
+                inputs.hyprland.follows = "hyprland";
+        };
+        # home manager setup 
+        home-manager = {
+                url = "github:nix-community/home-manager/release-24.11";
+                inputs.nixpkgs.follows = "nixpkgs";
+        };
+        
+    };
 
-	outputs = {nixpkgs,home-manager,...}@inputs: {
-		nixosConfigurations = {
-                    laptop = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				# Import the previous configuration.nix we used,
-				# so the old configuration file still takes effect
-				./hosts/laptop
-				
-				# enable home-manager module
-				home-manager.nixosModules.home-manager
-				{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.austinl = import ./home;
-				}
-			];
-                    };
-                };
-	};
+    outputs = {nixpkgs,home-manager,...}@inputs: {
+        nixosConfigurations = {
+            laptop = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                        # Import the previous configuration.nix we used,
+                        # so the old configuration file still takes effect
+                        ./hosts/laptop
+                        
+                        # enable home-manager module
+                        home-manager.nixosModules.home-manager
+                        {
+                                home-manager.useGlobalPkgs = true;
+                                home-manager.useUserPackages = true;
+                                home-manager.users.austinl = import ./home;
+                                home-manager.backupFileExtension = "hm-backup"; #allow home-manager to override files and back them up
+                        }
+                ];
+            };
+        };
+    };
 }
--- home/hyprland/config/rules.nix
+++ home/hyprland/config/rules.nix
@@ -3,7 +3,7 @@
 wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
         # IGNORE MAXIMIZE REQUESTS FROM APPS
-        "supressevent maximize, class:.*"
+        "suppressevent maximize, class:.*"
 
         # FIX SOME DRAGGING ISSUES WITH WAYLAND
         "nofocus, class:^$,title:^$,xwayland:1,floating:1, fullscreen:0, pinned:0"
