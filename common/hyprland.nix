57a6697f2eaed4386d0b2ca931597f88d37fe383
= <=>
2025-02-16 20:34:44 -0500

    made desktop work and moved more stuff to common modules

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,11 +1,10 @@
-{pkgs,...}:
+{inputs, pkgs,...}:
 {
     programs.hyprland = {
         enable = true;
         package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
-    programs.hyprpaper.enable = true;
     environment.systemPackages = [pkgs.hyprpaper];
 
     xdg.portal = {
