297fca33d14e05205c2bb01692450d5f3a66d3ae
Austinll0 <Austinllafever@ymail.com>
2025-12-29 19:57:53 -0500

    add wallpaper

--- home/themes/zen/default.nix
+++ home/themes/zen/default.nix
@@ -1,6 +1,6 @@
 {config,lib,...}:
 let 
-    enabled = config.theme == "zen"
+    enabled = config.theme == "zen";
 in
 {
     config = lib.mkIf enabled {
@@ -9,27 +9,27 @@ in
 
         home.file.".config/hypr/pimp.conf".source = ./hypr/pimp.conf;
         home.file.".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
-        home.file.".config/hypr/deco".source = {
-            source = ./hypr/deco
+        home.file.".config/hypr/deco" = {
+            source = ./hypr/deco;
             recursive = true;
-        }
-        home.file.".config/hypr/scripts".source = {
-            source = ./hypr/scripts
+        };
+        home.file.".config/hypr/scripts" = {
+            source = ./hypr/scripts;
             recursive = true;
-        }
+        };
         home.file.".config/rofi" = {
-            source = ./rofi
+            source = ./rofi;
             recursive = true;
-        }
+        };
 
         home.file.".config/waybar" = {
-            source = ./waybar
+            source = ./waybar;
             recursive = true;
-        }
+        };
 
         home.file.".config/kitty" = {
-            source = ./kitty
+            source = ./kitty;
             recursive = true;
-        }
+        };
     };
 }
