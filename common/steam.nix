28f37604dddaebed159de676ceeca5309d3884cd
= <=>
2026-01-04 00:11:44 -0500

    setup gamescope for steam

--- common/steam.nix
+++ common/steam.nix
@@ -4,11 +4,18 @@
     config = lib.mkIf config.steamModule.enable {
         programs.steam = {
             enable = true;
+            gamescopeSession.enable = false; #keep Hyprland as compositor
             remotePlay.openFirewall = true;
             dedicatedServer.openFirewall = true;
             localNetworkGameTransfers.openFirewall = true;
         };
-        environment.systemPackages = with pkgs; [protonup-qt];
+
+        environment.systemPackages = with pkgs; [
+        protonup-qt
+        gamescope
+        ];
+
+        programs.gamemode.enable = true;
     };
 
 }
